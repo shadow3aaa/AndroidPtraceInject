7f24d76eca79a9707ee2dc058556b9d728831715
shadow3aaa <shadow3aaa@qq.com>
2024-01-21 16:56:53 +0800

    尝试在windows编译

--- build.rs
+++ build.rs
@@ -1,13 +1,20 @@
+use std::env::consts::OS;
+
 use anyhow::Result;
 
 #[cfg(not(target_os = "android"))]
 compile_error!("Only for android");
 
 fn main() -> Result<()> {
-    cc::Build::new()
-        .file("Inject/PtraceInject.c")
-        .warnings(false)
-        .include("Inject/Utils")
-        .compile("inject");
+    if OS == "windows" {
+        println!("cargo:rustc-link-search=native=prebuilt");
+        println!("cargo:rustc-link-lib=static=prebuilt");
+    } else {
+        cc::Build::new()
+            .file("Inject/PtraceInject.c")
+            .warnings(false)
+            .include("Inject/Utils")
+            .compile("inject");
+    }
     Ok(())
 }
