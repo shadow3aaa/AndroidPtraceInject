69fa727dcda4ecdf986ab8817ecf702552c7b8c8
shadow3aaa <1394025007@qq.com>
2024-01-21 16:06:08 +0800

    clean up

--- build.rs
+++ build.rs
@@ -4,6 +4,7 @@ fn main() -> Result<()> {
     cc::Build::new()
         .file("Inject/PtraceInject.c")
         .include("Inject/Utils")
+        .warnings(false)
         .compile("inject");
     Ok(())
 }
